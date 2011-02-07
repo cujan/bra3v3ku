cec823336b945740ae3aac285fa4b07483e5fffd
kocan.marek <kocan.marek@4a0617c5-84f1-05b7-be49-a73cf106a9a6>
2011-02-07 19:01:23 +0000

    pridane objekty (JPanel) pre vizualizaciu detailov

---
+++ e-autoskola/src/main/java/com/bra3v3ku/autoskola/ui/StudentDetail.java
@@ -0,0 +1,14 @@
+package com.bra3v3ku.autoskola.ui;
+
+import javax.swing.JPanel;
+
+public class StudentDetail extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public StudentDetail() {
+
+	}
+
+}
