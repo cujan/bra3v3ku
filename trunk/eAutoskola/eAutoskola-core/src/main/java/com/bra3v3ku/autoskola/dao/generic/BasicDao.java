6051bc695fcd22da77db91fe1b99fa6ea814d1c4
kocan.marek <kocan.marek@4a0617c5-84f1-05b7-be49-a73cf106a9a6>
2011-02-14 22:17:24 +0000

    pridane dalsie metody do BasicDao. Potrebuju doladit, zatial nefunguju uplne ako maju

--- trunk/eAutoskola/eAutoskola-core/src/main/java/com/bra3v3ku/autoskola/dao/generic/BasicDao.java
+++ trunk/eAutoskola/eAutoskola-core/src/main/java/com/bra3v3ku/autoskola/dao/generic/BasicDao.java
@@ -6,4 +6,7 @@ import java.util.List;
 public interface BasicDao<T,ID extends Serializable> {
 	T getEntityByID(Object entityType,ID entityID);	
 	List<T> getEntityList(Object entityType);
+	void insert(T entity);
+	void update(T entity);
+	void delete(T entity);
 }
