63eacbe73e31d6845a899118c2f5864691b6635c
kocan.marek <kocan.marek@4a0617c5-84f1-05b7-be49-a73cf106a9a6>
2011-02-15 17:50:55 +0000

    mensie upravy na classe

--- trunk/eAutoskola/eAutoskola-core/src/main/java/com/bra3v3ku/autoskola/dao/generic/BasicDao.java
+++ trunk/eAutoskola/eAutoskola-core/src/main/java/com/bra3v3ku/autoskola/dao/generic/BasicDao.java
@@ -4,8 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface BasicDao<T,ID extends Serializable> {
-	T getEntityByID(Object entityType,ID entityID);	
-	List<T> getEntityList(Object entityType);
+	T getEntityByID(Class<T> entityType,ID entityID);	
+	List<T> getEntityList(Class<T> entityType);
 	void insert(T entity);
 	void update(T entity);
 	void delete(T entity);
--- trunk/eAutoskola/eAutoskola-core/src/test/java/com/bra3v3ku/autoskola/dao/generic/BasicDaoImplTest.java
+++ trunk/eAutoskola/eAutoskola-core/src/test/java/com/bra3v3ku/autoskola/dao/generic/BasicDaoImplTest.java
@@ -12,30 +12,13 @@ public class BasicDaoImplTest {
 	private static final String PERSISTENCE_UNIT_NAME = "eAutoskolaDerby"; 
 	
 	@Test
-	public void testGetEntityByID() {
-		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>( PERSISTENCE_UNIT_NAME );
-		Automobil auto  = autoDao.getEntityByID(new Automobil(),1);
-		assertEquals(auto.getZnacka(), "Skoda");		
-	}
-
-	@Test
-	public void testGetEntityList() {
-		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>( PERSISTENCE_UNIT_NAME );
-		List<Automobil> auto =  autoDao.getEntityList(new Automobil());
-		for (Automobil a : auto){
-			System.out.println("ID: "+a.getId()+" Znacka: "+a.getZnacka()+ " Model:" + a.getModel() );			
-		}
-		assertTrue(auto.size()==4);			
-	}
-	
-	@Test
 	public void testInsert() {
 		Automobil auto= new Automobil();
 		auto.setModel("polo");
-		auto.setZnacka("vw");
+		auto.setZnacka("vw1");
 		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>(PERSISTENCE_UNIT_NAME);
 		autoDao.insert(auto);
-		List<Automobil> auto1 =  autoDao.getEntityList(new Automobil());
+		List<Automobil> auto1 =  autoDao.getEntityList(Automobil.class);
 		for (Automobil a: auto1){
 			if (a.getZnacka()=="vw"){
 				assertTrue(true);
@@ -47,27 +30,47 @@ public class BasicDaoImplTest {
 	public void testUpdate() {
 		Automobil auto= new Automobil();
 		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>(PERSISTENCE_UNIT_NAME);
-		auto = autoDao.getEntityByID(new Automobil(), 5);
-		auto.setZnacka("polo1");
+		auto = autoDao.getEntityByID(Automobil.class, 0);
+		auto.setZnacka(auto.getZnacka()+"_updated");
 		autoDao.update(auto);
-		List<Automobil> auto1 =  autoDao.getEntityList(new Automobil());
+		List<Automobil> auto1 =  autoDao.getEntityList(Automobil.class);
 		for (Automobil a: auto1){
-			if (a.getModel()=="polo1"){
+			if (a.getModel().contains("_updated")){
 				assertTrue(true);
 			}
 		}
 	}
+	
+	
+	@Test
+	public void testGetEntityByID() {
+		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>( PERSISTENCE_UNIT_NAME );
+		Automobil auto  = autoDao.getEntityByID(Automobil.class,0);
+		assertEquals(auto.getZnacka(), "Skoda");		
+	}
+
+	@Test
+	public void testGetEntityList() {
+		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>( PERSISTENCE_UNIT_NAME );
+		List<Automobil> auto =  autoDao.getEntityList(Automobil.class);
+		for (Automobil a : auto){
+			System.out.println("ID: "+a.getId()+" Znacka: "+a.getZnacka()+ " Model:" + a.getModel() );			
+		}
+		assertTrue(auto.size()>0);			
+	}
+
 
 	@Test
 	public void testDelete() {
 		Automobil auto= new Automobil();
 		BasicDao<Automobil,Integer> autoDao = new BasicDaoImpl<Automobil, Integer>(PERSISTENCE_UNIT_NAME);
-		auto = autoDao.getEntityByID(new Automobil(), 6);
+		Automobil a1= autoDao.getEntityByID(Automobil.class,0);;
+		auto = autoDao.getEntityByID(Automobil.class,0 );
 		autoDao.delete(auto);
-		List<Automobil> auto1 =  autoDao.getEntityList(new Automobil());
+		List<Automobil> auto1 =  autoDao.getEntityList(Automobil.class);
 		Boolean isIncluded=false;
 		for (Automobil a: auto1){
-			if (a.getZnacka()=="vw"){
+			if (a.getZnacka()==a1.getZnacka()){
 				isIncluded=true;
 			}
 		}
