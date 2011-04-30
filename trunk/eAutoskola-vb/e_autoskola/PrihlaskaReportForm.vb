acd02073fd6d12e108ab5d3dc5c168178ef29c13
kocan.marek <kocan.marek@4a0617c5-84f1-05b7-be49-a73cf106a9a6>
2011-04-30 17:56:35 +0000

    form na "prihlaska - report"

---
+++ trunk/eAutoskola-vb/e_autoskola/PrihlaskaReportForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class PrihlaskaReportForm
+
+    Private Sub Form3_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'eAutoskolaDBDataSet.prihlaska' table. You can move, or remove it, as needed.
+        Me.prihlaskaTableAdapter.Fill(Me.eAutoskolaDBDataSet.prihlaska)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
